cf835b0d877260b1a7b1be908c5163bdddfdcea9
Subraiz <subraizahmed@gmail.com>
2018-10-06 00:08:27 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var script = document.createElement('script');
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+$("#submit-button").click(() => {
+    let email = $("#text-input").val()
+    let suffix = email.substr(email.length - 3)
+    if(suffix.toUpperCase() == "EDU"){
+        console.log(email)
+    }
+    
+})
