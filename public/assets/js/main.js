0d52cdd809da0dbe02f22f476ca9c95bd41a0280
Subraiz <subraizahmed@gmail.com>
2018-10-06 18:51:13 -0400

    Web Update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,6 +4,7 @@ script.src = '//code.jquery.com/jquery-1.11.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 
+
 $("#submit-button").click(() => {
     let email = $("#text-input").val()
     let suffix = email.substr(email.length - 3)
