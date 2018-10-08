89aa5eaf03f0505c573a08918441a4af12bfb910
Subraiz <subraizahmed@gmail.com>
2018-10-08 13:28:38 -0400

    Revert "Database Linked"
    This reverts commit bc59dfe0fdfd2618421d1fb4d6275926f074e53b.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,19 +1,7 @@
 var script = document.createElement('script');
-
+ 
 script.src = '//code.jquery.com/jquery-1.11.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
-
-
-var config = {
-  apiKey: "AIzaSyAAeuyg8vOKzrFOkS-oeNBTGvSTiWz-y2E",
-  databaseURL: "https://univents-a5f76.firebaseio.com/"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-var emails = database.ref().child('emails');
-console.log(emails);
-
 
 
 
@@ -21,8 +9,12 @@ $("#submit-button").click(() => {
     let email = $("#text-input").val()
     let suffix = email.substr(email.length - 3)
     if(suffix.toUpperCase() == "EDU"){
-       emails.set([email])
-        console.log("Email sent to database");
+        console.log(email)
     }
+<<<<<<< HEAD
     $("#text-input").val("");
 })
+=======
+    
+})
+>>>>>>> parent of bc59dfe... Database Linked
