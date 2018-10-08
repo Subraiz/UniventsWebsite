17c359348cba63f54a156b1d2867f05b482064df
Subraiz <subraizahmed@gmail.com>
2018-10-08 13:41:25 -0400

    Thank God I fixded this (stable for now)

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,20 +1,23 @@
-var script = document.createElement('script');
- 
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 
+var config = {
+  apiKey: "AIzaSyAAeuyg8vOKzrFOkS-oeNBTGvSTiWz-y2E",
+  databaseURL: "https://univents-a5f76.firebaseio.com/"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var emailList = database.ref().child('emails');
 
 $("#submit-button").click(() => {
     let email = $("#text-input").val()
     let suffix = email.substr(email.length - 3)
     if(suffix.toUpperCase() == "EDU"){
-        console.log(email)
+       firebase.database().ref('/emails/').once('value').then(function(snapshot) {
+        var currentDatabase = snapshot.val()
+        currentDatabase.push(email);
+        emailList.set(currentDatabase);
+    })
     }
-<<<<<<< HEAD
     $("#text-input").val("");
 })
-=======
-    
-})
->>>>>>> parent of bc59dfe... Database Linked
